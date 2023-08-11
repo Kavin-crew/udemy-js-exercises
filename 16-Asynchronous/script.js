541a51e3b812126522a1b3beb091f0553de6d83d
Kavin Abregana <abreganakavin@gmail.com>
2023-08-11 23:38:18 +0800

    coding challenge#2

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -75,4 +75,85 @@ const getCountry = function (...countries) {
   });
 };
 
-getCountry('usa', 'portugal', 'ph', 'germany');
+getCountry('usa', 'portugal', 'ph', 'germany', 'japan');
+
+// ////////////////////////////////////////////////////////////////////////
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You Win!');
+//     } else {
+//       reject(new Error('You lost your money!'));
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromise
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+// // Promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1);
+//   })
+//   .then(() => console.log('I waited for 1 second'));
+
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+/////////////////////////////////////////////////
+///// Coding challenge#2
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// global variable
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  // return new promise
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
